97c6cbf4debf544da83df28f3d03ad940cac7366
JosGon615 <josgon615@alu.centroafuera.es>
2023-02-06 14:14:03 +0100

    6

--- app/src/main/java/com/example/a7clases/Juego.kt
+++ app/src/main/java/com/example/a7clases/Juego.kt
@@ -2,10 +2,17 @@ package com.example.a7clases
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import com.example.a7clases.databinding.ActivityJuegoBinding
+import com.example.a7clases.databinding.ActivityResumenBinding
 class Juego : AppCompatActivity() {
+    private lateinit var binding: ActivityJuegoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityJuegoBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_juego)
+
+
     }
+
+
 }
